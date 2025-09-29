7dbab173046aa954b1147c768f2a1fa92ddd3ebe
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-29 12:52:03 -0400

    Criação dos produtos e categorias - MVC

---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
+
+@Entity({ name: 'tb_produtos' })
+export class Produto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
+  preco: number;
+
+  @Column({ type: 'int', nullable: false })
+  quantidade: number;
+
+  @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+    onDelete: 'CASCADE',
+  })
+  categoria: Categoria;
+}
