ee87f46e139c964201ead7dd0403930884d1d999
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-29 14:12:20 -0400

    Alterando o nome da tabela

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Produto } from '../../produto/entities/produto.entity';
 
-@Entity({ name: 'tb_categoria' })
+@Entity({ name: 'tb_categorias' })
 export class Categoria {
   @PrimaryGeneratedColumn()
   id: number;
